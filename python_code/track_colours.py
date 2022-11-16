95c97de165ed800b52515e5757975ccf26471689
Parth <pp537@cam.ac.uk>
2022-11-16 12:33:24 +0000

    commit

--- python_code/track_colours.py
+++ python_code/track_colours.py
@@ -1,34 +1,42 @@
 import cv2
 import numpy as np
+import imutils
  
 cam = cv2.VideoCapture("http://localhost:8081/stream/video.mjpeg")
 kernel = np.ones((5 ,5), np.uint8)
  
 while (True):
     __, frame = cam.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Red color
-    low_red = np.array([150, 75, 25])
-    high_red = np.array([180, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-    red = cv2.bitwise_and(frame, frame, mask=red_mask)#
-
+    crop = frame[240:550, 210:350]
+    hsv_frame = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
+#
     # Blue color
-    low_blue = np.array([100, 150, 4])
+    low_blue = np.array([100, 125, 4])
     high_blue = np.array([140, 255, 255])
     blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
-    blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
-    # Green color   
-    low_green = np.array([70, 50,4])
-    high_green = np.array([110, 255, 255])
-    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-    green = cv2.bitwise_and(frame, frame, mask=green_mask)
+    blue = cv2.bitwise_and(crop, crop, mask=blue_mask)
 
+    cnts1 = cv2.findContours(blue_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnts1 = imutils.grab_contours(cnts1)
+
+    for c in cnts1:
+        cX=0
+        cY=0
+        cv2.drawContours(crop,[c],-1,(0,255,0),3)
+        # compute the center of the contour
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])   
+            
+        cv2.circle(crop, (cX, cY), 7, (255, 255, 255), 1)
+        print(cX)
+        print(cY)
+
+    cv2.imshow("crop", crop)
     cv2.imshow("Frame", frame)
-    cv2.imshow("Red", red)
     cv2.imshow("Blue", blue)
-    cv2.imshow("Green", green)
+
     key = cv2.waitKey(1)
     if key == 27:
         break
