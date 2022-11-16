331026d50c1af69b5b634da9ee6ea0be40db4567
Parth Potdar <98214550+ParthVader02@users.noreply.github.com>
2022-11-16 09:47:33 +0000

    Add files via upload

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -34,48 +34,69 @@ void setup() {
 
 //Left
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  left->setSpeedFine(2000);
+  left->setSpeed(220);
   left->run(RELEASE);
 //RIght
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  right->setSpeedFine(2000);
+  right->setSpeed(150);
   right->run(RELEASE);
 }
 
-volatile int startFlag = 0;
-
+volatile int startFlag = 1;
+volatile int stop_follow = false;
 void loop() {
   //initialise static counters, for start sequence and junction count
-  static int junction_count = 0;
+  static int junction_count = 0; 
   static int start_count = 0;
   int line_detect = 0;
 
   //initialise sensors for readable code and read pins
-  int left_lineSensor = digitalRead(lineSensorpin_1); //8
-  int right_lineSensor = digitalRead(lineSensorpin_2); //9
-  int v_left_lineSensor = digitalRead(lineSensorpin_3); //10
-  int v_right_lineSensor = digitalRead(lineSensorpin_4); //11
-
+  int right_lineSensor = digitalRead(lineSensorpin_1); //8
+  int v_right_lineSensor = digitalRead(lineSensorpin_2); //9
+  int left_lineSensor = digitalRead(lineSensorpin_3); //10
+  int v_left_lineSensor = digitalRead(lineSensorpin_4); //11
+  delay(10);
+  Serial.println(v_left_lineSensor);
+  //Serial.println(right_lineSensor);
+  //Serial.println(v_left_lineSensor);
+  //Serial.println(v_right_lineSensor);
   //get to main line and rotate 90 deg clockwise
   if (startFlag == 0){
     if (left_lineSensor ==1 && right_lineSensor ==1 && v_left_lineSensor ==1 && v_right_lineSensor ==1){ // 1111
+    delay(100);
       start_count++;
+      Serial.println(start_count);
     }
-    if (left_lineSensor ==0 && right_lineSensor ==0 && v_left_lineSensor ==0 && v_right_lineSensor ==0){ //0000
-      line_detect = 1; //main line detected
-    }
-    start_to_line(start_count, line_detect);
+    start_to_line(start_count, left_lineSensor);
   }
   
   if(startFlag ==1){
 
-  //line follow algorithm, while checking for junctions
+  //Serial.println("start_up done");
 
-  //if junctions seen, do things
+  // Query PC if blocks are seen from camera, and which junction they are at
 
-  //if block there, run ultrasound sensor, LED to show type, save type to memory
-  // to check if block exists -> CV
+  //line follow algorithm, while checking for junctions#
 
+  if(right_lineSensor ==1  && v_right_lineSensor ==1){
+    junction_count++;
+  }
+
+  else if(left_lineSensor ==1  && v_left_lineSensor ==1){
+    junction_count++;
+  }
+
+  else if(left_lineSensor ==1 && right_lineSensor ==1 && v_left_lineSensor ==1 && v_right_lineSensor ==1){
+    junction_count++;    
+  }
+  
+  while (stop_follow != true){
+    junction(junction_count);
+    line_follow(left_lineSensor, right_lineSensor, v_left_lineSensor, v_right_lineSensor);
+  }
+  
+  //run ultrasound sensor, LED to show type, save type
+  ultrasonic_read()
   //run servo motor to capture block
 
   //go back to main line and follow line again 
@@ -89,27 +110,110 @@ void loop() {
   }
 }
 
-void line_follow(){  
+void line_follow(int left_lineSensor, int right_lineSensor, int v_left_lineSensor, int v_right_lineSensor){
+  int kp = 50;
+  int error = getError(left_lineSensor, right_lineSensor, v_left_lineSensor, v_right_lineSensor);
+  Serial.println(error);
+  static int left_speed;
+  static int right_speed;
 
-}
+  left_speed = 200 - kp*error;
+  right_speed = 150 + kp*error;
 
-void start_to_line(int count, int line_detect){
-  //go forward until 1111
-  left->run(BACKWARD);
-  right->run(FORWARD);
-
-  if(count == 1){ //line follow between start and main line
-    line_follow();
+  if(left_speed >255){
+    left_speed = 255;
+  }
+  if(right_speed >255){
+    right_speed = 255;
   }
 
-  else if(count ==2){ //once main line reached
-    left->run(RELEASE); //stop
-    right->run(RELEASE);
+  left->setSpeed(left_speed);
+  right->setSpeed(right_speed);
+  
+  left->run(FORWARD);
+  right->run(BACKWARD);
+}
 
-    while(line_detect !=1){
-      left->run(BACKWARD); //rotate until line detected
-      right->run(FORWARD);
+void start_to_line(int count, int left_lineSensor){
+  //go forward until 1111
+  if(count!=2){
+Serial.println("GO FORWARD");
+  left->run(FORWARD);
+  right->run(BACKWARD);
+  }
+  else if(count ==2){ //once main line reached
+    if(left_lineSensor !=1){
+       left->run(FORWARD); //rotate until line detected
+      right ->run(BACKWARD);
     }
-    startFlag = 1;
+    else{
+    left ->fullOff();
+    right ->fullOff();
+     startFlag = 1;
+    }
+  }
+}
+
+int getError(int a, int b, int c, int d){
+  int error;
+  if(a==0 && b==0 && c==0 && d==0){
+    error = 0;    
+  }
+  else if(a==1 && b==0 && c==0 && d==0){
+    error = 2;
+  }
+  else if(a==0 && b==0 && c==1 && d==0){
+    error = 4;
+  }
+  else if(a==0 && b==1 && c==0 && d==0){
+    error = -2;
+  }
+  else if(a==0 && b==0 && c==0 && d==1){
+    error = -4;
+  }
+  return error;
+}
+
+void junction(int count){
+  
+}
+
+void ultrasonic_read() {
+  long duration;
+  int distance[100];
+
+  for (int i = 0; i < 100; i++) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    //calculate duration of pulse and distance to object
+    duration = pulseIn(echoPin, HIGH);
+
+    //check if input is valid, if not then loop again
+    int dis = duration * 0.034/2;
+    if (dis < 60) {
+      distance[i] = dis;
+    } else {
+      i--;
+    }
+  }
+
+  //calculate mean distance
+  int mean_distance = 0;
+  for (int i = 0; i < 100; i++) {
+    mean_distance += distance[i];
+  }
+  mean_distance = mean_distance / 100;
+
+  // turn red or green LED on
+  if (mean_distance < 15) {
+      digitalWrite(greenLedPin, HIGH);
+      digitalWrite(redLedPin, LOW);
+  } else {
+      digitalWrite(greenLedPin, LOW);
+      digitalWrite(redLedPin, HIGH);
   }
 }
