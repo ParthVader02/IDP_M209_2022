e3db660608f94ae0213f7c7521215a36f947940a
OlulanaOsibeluwo <46073960+OlulanaOsibeluwo@users.noreply.github.com>
2022-11-22 10:42:21 +0000

    also add if statement to loop

---
+++ Arduino/junction_function.ino
@@ -0,0 +1,22 @@
+//in loop 
+if (junction_count == 5){
+    junction_count = 0;
+  }
+
+junction(int count, bool hasBlock, int blockType, int desiredJunction){
+  if (hasBlock == false){
+    if (count == 2 && desiredJunction == 2){
+      //turn left, pickup function
+      // find coordinates of blue block 1 in x,y
+      //if no blue in 2's xy co-ords continue forward
+    }
+    else if (count == 3 && desiredJunction == 3){
+      //turn right, pickup function
+      // find coordinates of blue block 2 in x,y
+      //if no blue in 2's xy co-ords continue forward
+    }
+    else if (count == 4 && desiredJunction == 4)
+    { //turn left, pickup function
+      // find coordinates of blue block 3 in x,y
+      //if no blue in 2's xy co-ords continue forward}
+
